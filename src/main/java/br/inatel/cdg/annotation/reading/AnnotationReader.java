f732f0175391b469a7ae05ec3d9fe2ee7c5d4a42
Jeferson Ferreira <jeferson@yoga.2>
2020-11-30 21:12:40 -0300

    Implementacao das Classes e Anotacoes

--- src/main/java/br/inatel/cdg/annotation/reading/AnnotationReader.java
+++ src/main/java/br/inatel/cdg/annotation/reading/AnnotationReader.java
@@ -1,19 +1,38 @@
 package br.inatel.cdg.annotation.reading;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 //Nessa classe você deverá implementar a sua solução
 //Adicionalmente você também deverá criar
 //a anotação NumberToSum como parte da solução
 
 public class AnnotationReader<T> {
+	
+	private int totalSum;
+	private Class<?> c;
 
 	public AnnotationReader(Class<?> clazz) {
+		this.c = clazz;
 	}
 
 	public void readAnnotations() {
+		// somando os valores das anotacoes dos metodos
+		for(Method m: c.getDeclaredMethods()) {
+		   if (m.isAnnotationPresent(NumberToSum.class)) { 
+			   totalSum += m.getAnnotation(NumberToSum.class).value();
+		   }
+		}
+		// somando os valores das anotacoes dos campos
+        for(Field f: c.getDeclaredFields()) {
+           if (f.isAnnotationPresent(NumberToSum.class)) {
+        	   totalSum += f.getAnnotation(NumberToSum.class).value();
+           }
+        }
 	}
 	
 	public int getTotalSum() {
-		return 0;
+		return totalSum;
 	}
 
 }
--- src/test/java/br/inatel/cdg/TesteAnnotationReading.java
+++ src/test/java/br/inatel/cdg/TesteAnnotationReading.java
@@ -2,8 +2,6 @@ package br.inatel.cdg;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Map;
-
 import org.junit.Test;
 
 import br.inatel.cdg.annotation.reading.AnnotationReader;
@@ -16,8 +14,8 @@ public class TesteAnnotationReading {
 	@Test
 	public void testeExemploClass1() {
 		
-		AnnotationReader aReader = 
-				new AnnotationReader(ExampleClass1.class);
+		AnnotationReader<Class<ExampleClass1>> aReader = 
+				new AnnotationReader<Class<ExampleClass1>>(ExampleClass1.class);
 		
 		aReader.readAnnotations();
 		
@@ -28,8 +26,8 @@ public class TesteAnnotationReading {
 	
 	@Test
 	public void testeExemploClass2() {
-		AnnotationReader aReader = 
-				new AnnotationReader(ExampleClass2.class);
+		AnnotationReader<Class<ExampleClass2>> aReader = 
+				new AnnotationReader<Class<ExampleClass2>>(ExampleClass2.class);
 		
 		aReader.readAnnotations();
 		
